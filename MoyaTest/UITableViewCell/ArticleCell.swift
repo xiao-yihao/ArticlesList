1634533db6bd05baf80e3ee754492974e20465f9
肖 怡豪 <t-igsho@mbp-15jau-046.yjoffice.local>
2019-10-09 21:02:47 +0900

    UI調整

--- MoyaTest/UITableViewCell/ArticleCell.swift
+++ MoyaTest/UITableViewCell/ArticleCell.swift
@@ -73,7 +73,7 @@ class ArticleCell: UITableViewCell {
 
             titleLabel.topAnchor.constraint(equalTo: titleView.topAnchor),
             titleLabel.leadingAnchor.constraint(equalTo: titleView.leadingAnchor, constant: 10),
-            titleLabel.trailingAnchor.constraint(equalTo: titleView.trailingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: titleView.trailingAnchor, constant: -10),
         ])
     }
 }
